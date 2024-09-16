e2282414b04b6946c6b50d102588d5a1008d7390
Bernardo <b.bernardo9815@gmail.com>
2024-09-15 22:32:26 -0300

    Adicao da barra progressiva

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,16 +1,50 @@
-import React from 'react'
-import Menu from '../../components/template/Menu';
+'use client'
 
+import React from 'react';
+import Menu from '../../components/template/Menu';
+import Titulo from '@/components/template/title_home';
+import { usePathname } from 'next/navigation';
 
 const LayoutHome = ({ children }) => {
+  const pathname = usePathname();
+  
+  // Define o título dinamicamente com base na rota
+  const getTitleAndStep = () => {
+    switch(pathname) {
+      case '/home/cadastrarpessoa/dadosbasicos':
+        return { titulo: "Cadastrar Pessoa", etapa: "Etapa (1)" };
+      case '/home/cadastrarpessoa/infoadd':
+        return { titulo: "Cadastrar Pessoa", etapa: "Etapa (2)" };
+      case '/home/cadastrarpessoa/endereco':
+        return { titulo: "Cadastrar Pessoa", etapa: "Etapa (3)" };
+      case '/home/cadastrarpessoa/contato':
+        return { titulo: "Cadastrar Pessoa", etapa: "Etapa (4)" };
+      case '/home/cadastrarpessoa/responsavel':
+        return { titulo: "Cadastrar Pessoa", etapa: "Etapa (5)" };
+      case '/home/cadastrarpessoa/concluido':
+        return { titulo: "Cadastrar Pessoa", etapa: "Concluído" };
+      case '/home/registraratividade':
+        return { titulo: "Registrar Pessoas", etapa: "" };
+      case '/home/cadastrarusuario':
+        return { titulo: "Cadastrar Usuário", etapa: "" };
+      default:
+        return { titulo: "Título Padrão", etapa: "" };
+    }
+  }
+
+  const { titulo, etapa } = getTitleAndStep();
+
   return (
     <div className='flex'>
       <Menu />
-      <div className=' w-full h-full'>
+      <div className='w-full h-full'>
+        <div className='w-full flex items-center justify-center'>
+          <Titulo titulo={titulo} etapa={etapa} />
+        </div>
         {children}
       </div>
     </div>
-  )
+  );
 }
 
-export default LayoutHome
+export default LayoutHome;
--- src/components/template/title_home.tsx
+++ src/components/template/title_home.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const title_home = (props: any) => {
     return (
-        <div className='bg-gray-700 w-full h-1/6 pt-24 pl-20 pb-28'>
-            <span className='text-3xl font-semibold pr-12 bg-red-500'>{props.titulo}</span>
+        <div className=' w-full h-1/6 pt-24 pl-20 pb-28'>
+            <span className='text-3xl font-semibold pr-12 '>{props.titulo}</span>
             <span className=' font-extralight'>{props.etapa}</span>
         </div>
     )
